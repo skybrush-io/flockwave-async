95cbab8119d8377701d5202ef9777dbd44e7ac0f
Tamas Nepusz <ntamas@gmail.com>
2025-04-20 14:40:46 +0200

    chore: bumped version to 2.0.0

--- src/flockwave/concurrency/version.py
+++ src/flockwave/concurrency/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave async utility package."""
 
-__version_info__ = (1, 4, 5)
+__version_info__ = (2, 0, 0)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
